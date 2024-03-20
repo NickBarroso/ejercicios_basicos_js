e97be81528714e36357ba136cbeb8f5757ba8afd
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2024-03-20 11:42:40 +0000

    Realizado primer ejercicio.

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+//1.1 Define tu superhéroe favorito.
+let superhero = "Hulk";
+
+//1.2 Define un número.
+let numeroFavorito = 50;
+
+//1.3 Define las dimensiones:
+let altura = 5;
+let longitud = 10;
+
+//1.4 Calcula la suma:
+let suma = altura + longitud;
+
+//1.5 Imprime los resultados.
+
+console.log(`\nMi héroe favorito es ${superhero}.\n
+Mi número favorito es ${numeroFavorito}.\n
+Mi altura es ${altura}, y mi longitud ${longitud}, todo esto suma: ${suma}\n`);
